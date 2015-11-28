2866b3026be1de300faa4f83172039b9ed4d7396
mostekcm <jrstone1>
2015-11-28 16:03:26 +0000

    closes: #2
    initial seed framework in place, I can now log in.  On to the framework piece.

---
+++ login/login.js
@@ -0,0 +1,17 @@
+angular.module( 'sample.login', [
+  'auth0'
+])
+.controller( 'LoginCtrl', function HomeController( $scope, auth, $location, store ) {
+
+  $scope.login = function() {
+    auth.signin({}, function(profile, token) {
+      store.set('profile', profile);
+      store.set('token', token);
+      $location.path("/");
+    }, function(error) {
+      alert(error.toString());
+      console.log("There was an error logging in", error);
+    });
+  }
+
+});
