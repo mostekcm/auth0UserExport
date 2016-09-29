c0bc7e46c8c09ce92760deb9171dcac2f8e3a84b
Carlos Mostek <carlos.mostek@auth0.com>
2016-09-29 10:18:44 -0500

    put app controller back, just kill the event callback

--- public/js/app.js
+++ public/js/app.js
@@ -69,13 +69,13 @@ angular.module('sample', [
 
         });
     })
-    // .controller('AppCtrl', function AppCtrl($scope, $location) {
-    //     $scope.$on('$routeChangeSuccess', function (e, nextRoute) {
-    //         if (nextRoute.$$route && angular.isDefined(nextRoute.$$route.pageTitle)) {
-    //             $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample';
-    //         }
-    //     });
-    // })
+    .controller('AppCtrl', function AppCtrl($scope, $location) {
+        // $scope.$on('$routeChangeSuccess', function (e, nextRoute) {
+        //     if (nextRoute.$$route && angular.isDefined(nextRoute.$$route.pageTitle)) {
+        //         $scope.pageTitle = nextRoute.$$route.pageTitle + ' | Auth0 Sample';
+        //     }
+        // });
+    })
 
 ;
 
